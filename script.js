bc34ecfee7d06a9d806954388b10a0b46b5e41e0
shanikumar-dev <shanik825299@gmail.com>
2026-05-02 16:05:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    // delete button
+    let btn = document.createElement("button");
+    btn.textContent = "Delete";
+    btn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(btn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
